d0de64c06d9d3d8f739fddfeb495a8d3b170e1f0
hiro-lapis <coolish555ddddd@gmail.com>
2022-11-09 22:47:13 +0900

    step3

--- app/fizzbuzz/main_test.go
+++ app/fizzbuzz/main_test.go
@@ -4,6 +4,12 @@ import "testing"
 
 func TestFizzbuzz(t *testing.T) {
 	// 3の時はFizzになるか
+	v := Fizzbuzz(3)
+
+	if v != "Fizz" {
+		t.Error("Expected Fizz", v)
+	}
+
 	// 5の時はBuzzになるか
 	// 15の時はFizzBuzzになるか
 	// 7の時は7になるか
