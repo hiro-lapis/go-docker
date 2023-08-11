f21dcac24d42a295234cebf75bdaf43cc5868b18
hiro-lapis <coolish555ddddd@gmail.com>
2023-08-11 13:26:06 +0900

    :memo: configuration CORS on Graphql server hanlder

--- app/cmd/server.go
+++ app/cmd/server.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/rs/cors"
 	"go_docker/graphql/generated"
 	"go_docker/graphql/resolver"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 const defaultPort = "8080"
@@ -20,8 +20,13 @@ func main() {
 	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	// frontend connection, setting CORS
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:4000"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+	})
+	http.Handle("/playground", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", corsMiddleware.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
