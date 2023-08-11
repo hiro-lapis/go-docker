5a53ac9bab16d0a16591e7a743410f6d493740dc
hiro-lapis <coolish555ddddd@gmail.com>
2023-08-11 13:27:38 +0900

    :wrench: nuxt cofiguration to ssr

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import { defineNuxtConfig } from 'nuxt/config'
 
+export default defineNuxtConfig({
+  ssr: true,
+  modules: ['@nuxtjs/apollo'],
+
+  apollo: {
+    clients: {
+      default: {
+        // Graphql endpoint
+        httpEndpoint: 'http://localhost:8080/query',
+        // Rest endpoint
+        // httpEndpoint: 'https://localhost:8080',
+      }
+    },
+  },
 })
